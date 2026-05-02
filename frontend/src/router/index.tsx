b85b791fcafbd57eedf878853a5de7ac83cea752
fercholink <fercho028890@gmail.com>
2026-05-01 22:02:30 -0500

    fix: remove unused Navigate import to fix build error

--- frontend/src/router/index.tsx
+++ frontend/src/router/index.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, Navigate } from 'react-router-dom';
+import { createBrowserRouter } from 'react-router-dom';
 import { ProtectedRoute } from '../components/ProtectedRoute';
 import LoginPage from '../pages/LoginPage';
 import RegisterPage from '../pages/RegisterPage';
